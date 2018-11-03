634917c06254cfbb900c202286341ceb8f8e69d2
Emiliano <e.tortorelli1991@gmail.com>
2018-11-03 13:41:51 -0300

    change to es6

--- binary-tree/binary-tree.js
+++ binary-tree/binary-tree.js
@@ -4,35 +4,8 @@ const _SMALLER = 2;
 const _ITERATIVE = 1;
 const _RECURSIVE = 2
 
-module.exports = {
-    getTree: _getTree,
-    push: _push,
-    printInOrder: _printInOrder,
-    printPreOrder: _printPreOrder,
-    printPosOrder: _printPosOrder,
-    countNodes: _countNodes,
-    setMode: _setMode,
-    BIGGER: _BIGGER,
-    SMALLER: _SMALLER,
-    modes: {
-        ITERARIVE: _ITERATIVE,
-        RECURSIVE: _RECURSIVE
-    }
-}
 
-
-
-let root = null;
-let mode = _RECURSIVE;
-
-function _setMode(_mode) {
-    if (root)
-        throw "Cannot set mode in an existing tree";
-    mode = _mode;
-}
-// var root = createBinaryTree(2);
-
-function createNewLeaf(value) {
+const createNewLeaf = (value) => {
     return {
         value: value,
         left: null,
@@ -40,14 +13,80 @@ function createNewLeaf(value) {
     }
 }
 
-function createBinaryTree(rootValue) {
-    return createNewLeaf(rootValue);
+const defaultPrint = (node) => {
+    console.log(node.value);
 }
+
+module.exports = class BinaryTree {
+    constructor(settings) {
+        this.settings = settings;
+        if (!this.settings.mode) {
+            this.settings.mode = _RECURSIVE;
+        }
+    }
+    push(value) {
+        if (!this.settings || !this.settings.compareFunction)
+            throw "You need to specify a compareFunction before push in the tree";
+
+        if (!this.root)
+            this.root = createNewLeaf(value);
+        else
+            pushInNode(this.root, value, this.settings.compareFunction);
+    }
+
+    printInOrder() {
+        inOrderSearch(this.root, this.settings.mode, this.settings.printFunction ? this.settings.printFunction : defaultPrint);
+    }
+
+    printInPosOrder() {
+        posOrderSearch(this.root, this.settings.mode, this.settings.printFunction ? this.settings.printFunction : defaultPrint);
+    }
+
+    printInPreOrder() {
+        preOrderSearch(this.root, this.settings.mode, this.settings.printFunction ? this.settings.printFunction : defaultPrint);
+    }
+
+    getSettings() {
+        if (this.settings)
+            return JSON.stringify(JSON.parse(this.settings));
+    }
+
+    countNodes() {
+        var count = 0;
+        inOrderSearch(this.root, this.settings.mode, () => count++);
+
+        return count;
+    }
+
+    //debug purposes
+    printTree(){
+        console.log(JSON.stringify(this.root));
+    }
+
+
+}
+
+function pushInNode(_node, _value, _comparefunction) {
+
+    if (_comparefunction(_node.value, _value) == _BIGGER) {
+        if (!_node.left) {
+            _node.left = createNewLeaf(_value);
+        }else {
+            pushInNode(_node.left,_value,_comparefunction);
+        }
+    } else {
+        if (!_node.right) {
+            _node.right = createNewLeaf(_value);
+        } else {
+            pushInNode(_node.right,_value,_comparefunction);
+        }
+    }
+}
+
 
 function inOrderSearchIterative(node, operation, params) {
     let parents = [];
     while (true && (!params || !params.stopSearch)) {
-        // console.log(parents);
         if (node) {
             parents.push(node);
             node = node.left;
@@ -65,7 +104,7 @@ function inOrderSearchIterative(node, operation, params) {
     }
 }
 
-function inOrderSearch(node, operation, params) {
+function inOrderSearch(node, mode, operation, params) {
     if (mode == _RECURSIVE) {
         inOrderSearchRecursive(node, operation, params);
     } else {
@@ -75,7 +114,7 @@ function inOrderSearch(node, operation, params) {
 
 
 //TODO: Make iterative
-function preOrderSearch(node, operation, params) {
+function preOrderSearch(node, mode, operation, params) {
     //StopSearch is totally optional. It should be set on "operation" function if required
     if (!node || (params && params.stopSearch))
         return;
@@ -83,19 +122,19 @@ function preOrderSearch(node, operation, params) {
     //This should do whatever you want to do with the visited node.
     operation(node, params);
 
-    preOrderSearch(node.left, operation, params);
-    preOrderSearch(node.right, operation, params);
+    preOrderSearch(node.left, mode, operation, params);
+    preOrderSearch(node.right, mode, operation, params);
 
 }
 
 //TODO: make iterative
-function posOrderSearch(node, operation, params) {
+function posOrderSearch(node, mode, operation, params) {
     //StopSearch is totally optional. It should be set on "operation" function if required
     if (!node || (params && params.stopSearch))
         return;
 
-    posOrderSearch(node.left, operation, params);
-    posOrderSearch(node.right, operation, params);
+    posOrderSearch(node.left, mode, operation, params);
+    posOrderSearch(node.right, mode, operation, params);
 
     //This should do whatever you want to do with the visited node.
     operation(node, params);
@@ -108,82 +147,11 @@ function inOrderSearchRecursive(node, operation, params) {
     if (!node || (params && params.stopSearch))
         return;
 
-    inOrderSearch(node.left, operation, params);
+    inOrderSearchRecursive(node.left, operation, params);
 
     //This should do whatever you want to do with the visited node.
     if (!params || !params.stopSearch) operation(node, params);
 
-    inOrderSearch(node.right, operation, params);
+    inOrderSearchRecursive(node.right, operation, params);
 
 }
-
-
-function pushInNode(_node, _value, _comparefunction) {
-
-    //I'm creating this operation function inside push because i wont be using it anywhere else.
-    var operation = function (node, params) {
-        if (params.compareFunction(node.value, params.value) == _BIGGER) {
-            if (!node.left) {
-                node.left = createNewLeaf(params.value);
-                params.stopSearch = true; // once the leaf is created, I do not need the search to continue
-            }
-        } else {
-            if (!node.right) {
-                node.right = createNewLeaf(params.value);
-                params.stopSearch = true; // once the leaf is created, I do not need the search to continue
-            }
-        }
-    }
-
-    var params = {
-        compareFunction: _comparefunction,
-        value: _value,
-        stopSearch: false
-    }
-    inOrderSearch(_node, operation, params);
-}
-
-function defaultPrint(node) {
-    console.log(node.value);
-}
-
-function _printInOrder(printFunction) {
-    inOrderSearch(root, printFunction ? printFunction : defaultPrint);
-}
-
-
-function _printPreOrder(printFunction) {
-    preOrderSearch(root, printFunction ? printFunction : defaultPrint);
-}
-
-
-function _printPosOrder(printFunction) {
-    posOrderSearch(root, printFunction ? printFunction : defaultPrint);
-}
-
-function _countNodes() {
-    var count = 0;
-    function countFunction() {
-        count++;
-    }
-    inOrderSearch(root, countFunction);
-    return count;
-}
-
-function _getTree() {
-    return root;
-}
-
-function _push(value, compareFunction) {
-    if (!root) {
-        root = createBinaryTree(value);
-        return;
-    }
-
-    if (!compareFunction) {
-        throw "You need to specify a compareFunction before push in the tree";
-    }
-
-    pushInNode(root, value, compareFunction);
-}
-
--- binary-tree/main.js
+++ binary-tree/main.js
@@ -2,28 +2,40 @@ const bts = require('./binary-tree');
 // let val = bts.hello(); // val is "Hello"   
 
 
+
 function compareInt(a, b) {
-    return a > b ? bts.BIGGER : bts.SMALLER;
+    return a > b ? 1 : 2;
 }
 
-// bts.push(4, compareInt);
-// bts.push(6, compareInt);
-// bts.push(7, compareInt);
-// bts.push(0, compareInt);
-// bts.push(1, compareInt);
+
+let settings = {
+    compareFunction: compareInt,
+    mode:2
+}
+
+const tree = new bts( settings);
+
+
+// tree.push(4, compareInt);
+// tree.push(6, compareInt);
+// tree.push(7, compareInt);
+// tree.push(0, compareInt);
+// tree.push(1, compareInt);
 // console.log(JSON.stringify(bts.getTree()));
 // console.log(JSON.stringify(bts.getTree()));
 // bts.setMode(bts.modes.ITERARIVE);
-console.time();
+// console.time();
 
-for(var i = 0; i < 300000 ; i++){
+for(var i = 0; i < 100 ; i++){
     // bts.push(1,compareInt);
-    bts.push(Math.floor(Math.random() *1000) ,compareInt);
+    tree.push(Math.floor(Math.random() *1000) ,compareInt);
 }
 
-console.timeEnd();
+// console.timeEnd();
 
-console.log(bts.countNodes());
+// console.log(tree.countNodes());
+tree.printInOrder();
+// tree.printTree();
 
 
 // function print() {
